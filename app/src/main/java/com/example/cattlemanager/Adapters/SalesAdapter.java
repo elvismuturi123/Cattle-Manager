040f5130ecf73cf47b9aadee3af5ae197c4fae61
Elvis <elvismuturi123>
2024-03-28 09:33:46 +0300

    Rectified UI

---
+++ app/src/main/java/com/example/cattlemanager/Adapters/SalesAdapter.java
@@ -0,0 +1,2 @@
+package com.example.cattlemanager.Adapters;public class SalesAdapter {
+}
---
+++ app/src/main/java/com/example/cattlemanager/Classses/MilkProduct.java
@@ -0,0 +1,47 @@
+package com.example.cattlemanager;
+
+public class MilkProduct {
+    String productID;
+    String cBreed;
+    String cDate;
+    String cTotal;
+    String cPrice;
+    String cNotes;
+
+    public MilkProduct() {
+
+    }
+
+    public MilkProduct(String productID, String cBreed, String cDate, String cTotal, String cPrice, String cNotes) {
+        this.productID = productID;
+        this.cBreed = cBreed;
+        this.cDate = cDate;
+        this.cTotal = cTotal;
+        this.cPrice = cPrice;
+        this.cNotes = cNotes;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public String getcBreed() {
+        return cBreed;
+    }
+
+    public String getcDate() {
+        return cDate;
+    }
+
+    public String getcTotal() {
+        return cTotal;
+    }
+
+    public String getcPrice() {
+        return cPrice;
+    }
+
+    public String getcNotes() {
+        return cNotes;
+    }
+}
